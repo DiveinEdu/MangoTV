08b3aa6efaed43c9544112451dd064903b12d84d
apple <apple@lishan.lan>
2015-01-12 11:06:01 +0800

    创建MangoTV项目

---
+++ MangoTV/MangoTV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MangoTV
+//
+//  Created by apple on 15/1/12.
+//  Copyright (c) 2015年 戴维营教育. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MangoTV/MangoTV/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MangoTV
+//
+//  Created by apple on 15/1/12.
+//  Copyright (c) 2015年 戴维营教育. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
